f1b83a72f032cadb20c051dbb18108ed3e8da496
caovanhoang63 <22520457@gm.uit.edu.vn>
2023-10-01 23:33:49 +0700

    Implement PexelsImageAPI.cs using Pexels API

---
+++ IT008-AppHocAV/IT008-AppHocAV/Services/PexelsImageAPI.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+using PexelsDotNetSDK.Api;
+
+namespace IT008_AppHocAV.Services
+{
+    public class PexelsImageAPI
+    {
+        public static async Task<BitmapImage> GetImages(string keyword)
+        {
+            var pexelsClient = new PexelsClient("9A5Ec3I5Td0wKHPHUQH7P1a9tS2bp8y3blhs1DY6YEzoYVr9CXwMQMD8");
+            var result = await pexelsClient.SearchPhotosAsync(keyword);
+            var photoSource = result.photos[0].source.large;
+            var image = new BitmapImage();
+            image.BeginInit();
+            image.UriSource = new Uri(photoSource); 
+            image.EndInit();
+            return image;
+        }
+    }
+}
