e5ee8ee0052f817b5bfd04b46bb041c4281efef8
caovanhoang63 <22520457@gm.uit.edu.vn>
2023-10-01 23:33:13 +0700

    Create DictionaryService and DictionaryEntry class to get word definitions from Dictionary API

---
+++ IT008-AppHocAV/IT008-AppHocAV/Models/DictionaryEntry.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+
+namespace IT008_AppHocAV.Models
+{
+    public class DictionaryEntry
+    {
+        public string word { get; set; } 
+        public string phonetic{ get; set; } 
+        public string origin{ get; set; } 
+        public List<Phonetic> phonetics{ get; set; } 
+        public List <Meaning> meanings { get; set; }
+        public string[] sourceUrls { get; set; }
+    }
+
+    public class Meaning
+    {
+        public string partOfSpeech{ get; set; } 
+        public string[] synonyms{ get; set; } 
+        public string[] antonyms{ get; set; } 
+    
+        public List<Definition> definitions { get; set; }
+    
+    }
+
+    public class Definition
+    {
+        public string definition { get; set; }
+        public string example{ get; set; } 
+        public string[] synonyms{ get; set; } 
+        public string[] antonyms{ get; set; } 
+    }
+
+    public class Phonetic
+    {
+        public string text;
+        public string audio;
+    }
+}
---
+++ IT008-AppHocAV/IT008-AppHocAV/Services/DictionaryAPI.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using DictionaryEntry = IT008_AppHocAV.Models.DictionaryEntry;
+
+namespace IT008_AppHocAV.Services
+{
+    public class DictionaryApi
+    {
+        public static async Task<List<DictionaryEntry>> SearchDictionary(string query)
+        {
+            Console.WriteLine("Searching " + query + "...");
+
+            using (HttpClient client = new HttpClient())
+            {
+                string url = $"https://api.dictionaryapi.dev/api/v2/entries/en/{query}";
+                try
+                {
+                    var response = await client.GetStringAsync(url);
+                    List<DictionaryEntry> result = JsonSerializer.Deserialize<List<DictionaryEntry>>(response);
+                    return result;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error: " + ex.Message);
+                    return null;
+                }
+            }
+        }
+    }
+}
