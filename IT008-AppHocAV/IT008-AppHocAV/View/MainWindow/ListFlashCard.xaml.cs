6ec1599a084c19a812a77463730d9b5a7f2e981c
caovanhoang63 <22520457@gm.uit.edu.vn>
2023-10-19 14:55:12 +0700

    create ListFlashCard.xaml, MakeFlashCard.xaml

---
+++ IT008-AppHocAV/IT008-AppHocAV/View/MainWindow/ListFlashCard.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace IT008_AppHocAV.View.MainWindow
+{
+    public partial class ListFlashCard : Page
+    {
+        public ListFlashCard()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ IT008-AppHocAV/IT008-AppHocAV/View/MainWindow/MakeFlashCard.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace IT008_AppHocAV.View.MainWindow
+{
+    public partial class MakeFlashCard : Page
+    {
+        public MakeFlashCard()
+        {
+            InitializeComponent();
+        }
+    }
+}
