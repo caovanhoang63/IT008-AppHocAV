6c8444c5eac6605a4ec2d63f63019c1cc3ab0c83
caovanhoang63 <22520457@gm.uit.edu.vn>
2023-10-25 21:10:18 +0700

    Create SignUp Window

---
+++ IT008-AppHocAV/IT008-AppHocAV/Models/UserInfo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace IT008_AppHocAV.Models
+{
+    public class UserInfo
+    {
+        // Thuộc tính (properties)
+        public string FullName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Gender { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+
+        public UserInfo()
+        {
+        }
+
+        public UserInfo(string fullName, DateTime dateOfBirth, string email, string phoneNumber, string gender, string userName, string password)
+        {
+            FullName = fullName;
+            DateOfBirth = dateOfBirth;
+            Email = email;
+            PhoneNumber = phoneNumber;
+            Gender = gender;
+            UserName = userName;
+            Password = password;
+        }
+        
+    }
+}
---
+++ IT008-AppHocAV/IT008-AppHocAV/View/SignUpWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+using IT008_AppHocAV.Models;
+
+namespace IT008_AppHocAV.View
+{
+    public partial class SignUpWindow : Window
+    {
+        public SignUpWindow(LoginWindow loginWindow)
+        {
+            InitializeComponent();
+            this._loginWindow = loginWindow;
+        }
+
+        private readonly LoginWindow _loginWindow;
+        
+        private void SignUpPage_OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void BtnClose_OnClick(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result =  MessageBox.Show("Are you sure want to continue?","",MessageBoxButton.YesNo);
+            if (result == MessageBoxResult.Yes)
+            {
+                _loginWindow.Show();
+                this.Close();
+            }
+        }
+
+        private void SignUpBtn_OnClick(object sender, RoutedEventArgs e)
+        {
+            UserInfo user = new UserInfo(
+                FullName.Text,
+                DateOfBirth.DisplayDate,
+                Email.Text,
+                PhoneNumber.Text,
+                "",
+                UserName.Text,
+                Password.Password
+                );
+            Console.WriteLine(user);
+        }
+    }
+}
