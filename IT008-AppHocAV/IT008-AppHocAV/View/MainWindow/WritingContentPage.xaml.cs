889d8b177391775c7623e8f9c4cf80c915763b12
caovanhoang63 <22520457@gm.uit.edu.vn>
2023-11-10 18:08:38 +0700

    create writing page

---
+++ IT008-AppHocAV/IT008-AppHocAV/View/MainWindow/WritingContentPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace IT008_AppHocAV.View.MainWindow
+{
+    public partial class WritingContentPage : Page
+    {
+        public WritingContentPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
