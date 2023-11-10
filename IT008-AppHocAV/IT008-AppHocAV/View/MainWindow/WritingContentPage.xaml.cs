d0b547553dd676f30f3a2ba6313468d96ea07ee7
caovanhoang63 <22520457@gm.uit.edu.vn>
2023-11-10 21:11:42 +0700

    handle create a new essay and navigate to WritingContent page

--- IT008-AppHocAV/IT008-AppHocAV/View/MainWindow/WritingContentPage.xaml.cs
+++ IT008-AppHocAV/IT008-AppHocAV/View/MainWindow/WritingContentPage.xaml.cs
@@ -1,12 +1,37 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Media;
+using IT008_AppHocAV.Models;
 
 namespace IT008_AppHocAV.View.MainWindow
 {
     public partial class WritingContentPage : Page
     {
-        public WritingContentPage()
+        private readonly IT008_AppHocAV.MainWindow _mainWindow;
+        private readonly WritingPage _writingPage;
+        public WritingContentPage(IT008_AppHocAV.MainWindow mainWindow, WritingPage writingPage)
         {
             InitializeComponent();
+            this._mainWindow = mainWindow;
+            this._writingPage = writingPage;
+        }
+
+        private void WritingContentPage_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            TitleTextBlock.Text = _writingPage.Essay.Title;
+            TopicTextBlock.Text = _writingPage.Essay.Topic;
+            
+            if (_writingPage.Essay.Image != null)
+            {
+                WritingImage.Source = _writingPage.Essay.Image;
+            }
+            else
+            {
+                WritingImage.Visibility = Visibility.Collapsed;
+            }
         }
     }
 }
--- IT008-AppHocAV/IT008-AppHocAV/View/MainWindow/WritingPage.xaml.cs
+++ IT008-AppHocAV/IT008-AppHocAV/View/MainWindow/WritingPage.xaml.cs
@@ -1,11 +1,23 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+using IT008_AppHocAV.Models;
+using Microsoft.Win32;
 
 namespace IT008_AppHocAV.View.MainWindow
 {
     public partial class WritingPage : Page
     {
-        private IT008_AppHocAV.MainWindow _mainWindow; 
+        private readonly IT008_AppHocAV.MainWindow _mainWindow;
+        private Essay _essay;
+        
+
+        public Essay Essay
+        {
+            get => _essay;
+        }
+
         public WritingPage()
         {
             InitializeComponent();
@@ -15,12 +27,44 @@ namespace IT008_AppHocAV.View.MainWindow
         {
             InitializeComponent();
             this._mainWindow = mainWindow;
+            _essay = new Essay();
+            _essay.ImagePath = null;
+            _essay.ImagePath = null;
         }
 
 
         private void StartWritingButton_OnClick(object sender, RoutedEventArgs e)
         {
             _mainWindow.NavigateToPage("WritingContentPage");
+
+            _essay.Title = TitleTextBox.Text;
+            _essay.Topic = TopicTextBox.Text;
+            _essay.Description = DescriptionTextBox.Text;
+            _essay.CreatedAt =  DateTime.Now; 
+            _essay.UpdatedAt =  DateTime.Now;
+        }
+
+
+        private void AddImageButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Files|*.jpg;*.jpeg;*.png;...";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string fileName = openFileDialog.FileName;
+                if (fileName != String.Empty)
+                {
+                    ImagePath.Text = fileName;
+                    BitmapImage image = new BitmapImage(new Uri(fileName));
+                    WritingImage.Source = image;
+                    ImagePath.Visibility = Visibility.Visible;
+                    WritingImage.Visibility = Visibility.Visible;
+
+                    _essay.ImagePath = ImagePath.Text;
+                    _essay.Image = image;
+
+                }
+            }
         }
     }
 }
