7e3231b69e1f75f6867aae16a93c3453f236520d
Hoàng Huy <146901880+DoHuy123@users.noreply.github.com>
2023-12-31 16:05:05 +0700

    change UI MessageBox

--- IT008-AppHocAV/IT008-AppHocAV/Repositories/DbConnection/DbConnection.cs
+++ IT008-AppHocAV/IT008-AppHocAV/Repositories/DbConnection/DbConnection.cs
@@ -39,7 +39,7 @@ namespace IT008_AppHocAV.Repositories.DbConnection
         #region Declare Constructors
         public DbConnection()
         {   
-            _sqlConnection = new SqlConnection(Settings.Default.LocalDBConnectionString);
+            _sqlConnection = new SqlConnection(Settings.Default.DbConnectionString);
             _authentication = new Authentication(_sqlConnection);
             _essayQ = new EssayQ(_sqlConnection);
             _deskQ = new DeskQ(_sqlConnection);
